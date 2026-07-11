a87568b8ed81fa907a73526d651e2caee162c31c
veliev nicat <vvnicat199java@yandex.ru>
2026-07-11 23:48:41 +0300

    fix: убрана ошибка const type

--- src/services/services-tokens.ts
+++ src/services/services-tokens.ts
@@ -10,8 +10,8 @@ export const getToken = (): Token => {
 
 export const saveToken = (token: Token): void =>{
   localStorage.setItem(AUTH_TOKEN_KEY_NAME, token); // cохраняем авторизованного пользователя
-}
+};
 //когда пользователь нажимает выйти
 export const dropToken= () : void => {
   localStorage.removeItem(AUTH_TOKEN_KEY_NAME); //стираем ячейку
-}
+};
